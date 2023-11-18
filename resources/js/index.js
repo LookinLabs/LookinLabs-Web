60987d54589997d001cea8d7cd0e8a8746a75704
Martin Sidorov <martin.sidorov@wallester.com>
2023-11-18 19:30:33 +0200

    Add navbar menu toggle for now

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,7 +1,13 @@
 // Initialization for ES Users
-import {
-    Carousel,
-    initTE,
-  } from "tw-elements";
-  
-  initTE({ Carousel });
+import { Carousel, initTE } from "tw-elements";
+
+initTE({ Carousel });
+
+document.getElementById("nav-toggle").addEventListener("click", function () {
+  var navMenu = document.getElementById("nav-menu");
+  if (navMenu.style.display === "none") {
+    navMenu.style.display = "flex";
+  } else {
+    navMenu.style.display = "none";
+  }
+});
