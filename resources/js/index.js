a4510b6efccdb8949e55896191d6dfcb7e909f02
Martin Sidorov <martin.sidorov@wallester.com>
2023-12-02 19:05:01 +0200

    Make the carousel to be scrollable

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,5 +1,3 @@
-// Some carousel script here
-
 document.getElementById("nav-toggle").addEventListener("click", function () {
   var navMenu = document.getElementById("nav-menu");
   if (navMenu.style.display === "none") {
@@ -8,3 +6,22 @@ document.getElementById("nav-toggle").addEventListener("click", function () {
     navMenu.style.display = "none";
   }
 });
+
+// Carousel buttons click event
+var leftCarouselButton = document.getElementById("left-carousel-button");
+var rightCarouselButton = document.getElementById("right-carousel-button");
+var carousel = document.getElementById("carousel");
+
+leftCarouselButton.onclick = function () {
+  carousel.scrollTo({
+    left: carousel.scrollLeft - carousel.clientWidth,
+    behavior: "smooth",
+  });
+};
+
+rightCarouselButton.onclick = function () {
+  carousel.scrollTo({
+    left: carousel.scrollLeft + carousel.clientWidth,
+    behavior: "smooth",
+  });
+};
