a516936ba3e1e8bba4d071b38fe4c4a5c23eda16
KostLinux <christofher.kost@lhv.ee>
2024-04-13 19:21:35 +0300

    Added terms and conditions button

--- resources/js/index.js
+++ resources/js/index.js
@@ -274,55 +274,61 @@ document
   .addEventListener("submit", function (event) {
     event.preventDefault();
 
-    // Show loader
-    document.getElementById("loader").classList.remove("hidden");
+    // Check if both checkboxes are checked
+    if(document.getElementById('termsCheckbox').checked && document.getElementById('privacyCheckbox').checked) {
+      // Show loader
+      document.getElementById("loader").classList.remove("hidden");
 
-    let formData = new FormData(this);
-    let username = formData.get("username");
-    let email = formData.get("email");
-    let title = formData.get("title");
-    let link = formData.get("link");
-    let description = formData.get("description");
-    let captcha_response = grecaptcha.getResponse();
+      let formData = new FormData(this);
+      let username = formData.get("username");
+      let email = formData.get("email");
+      let title = formData.get("title");
+      let link = formData.get("link");
+      let description = formData.get("description");
+      let captcha_response = grecaptcha.getResponse();
 
-    let templateParams = {
-      username: username,
-      email: email,
-      title: title,
-      link: link,
-      description: description,
-      "g-recaptcha-response": captcha_response,
-    };
+      let templateParams = {
+        username: username,
+        email: email,
+        title: title,
+        link: link,
+        description: description,
+        "g-recaptcha-response": captcha_response,
+      };
 
-    emailjs
-      .send("lookinlabs_zoho", "lookinlabs_template_id", templateParams)
-      .then(
-        function () {
-          // Hide contact form
-          document.getElementById("contactForm").classList.add("hidden");
+      emailjs
+        .send("lookinlabs_zoho", "lookinlabs_template_id", templateParams)
+        .then(
+          function () {
+            // Hide contact form
+            document.getElementById("contactForm").classList.add("hidden");
 
-          // Show success banner
-          let successBanner = document.getElementById("success-banner");
-          successBanner.classList.remove("hidden");
-          successBanner.classList.add("flex");
-          setTimeout(function () {
-            successBanner.classList.add("hidden");
-          }, 5000); // Hide after 5 seconds
-        },
-        function (error) {
-          // Show fail banner
-          let failBanner = document.getElementById("fail-banner");
-          failBanner.classList.remove("hidden");
-          failBanner.classList.add("flex");
-          setTimeout(function () {
-            failBanner.classList.add("hidden");
-          }, 5000); // Hide after 5 seconds
-        }
-      )
-      .finally(function () {
-        // Hide loader
-        document.getElementById("loader").classList.add("hidden");
-      });
+            // Show success banner
+            let successBanner = document.getElementById("success-banner");
+            successBanner.classList.remove("hidden");
+            successBanner.classList.add("flex");
+            setTimeout(function () {
+              successBanner.classList.add("hidden");
+            }, 5000); // Hide after 5 seconds
+          },
+          function (error) {
+            // Show fail banner
+            let failBanner = document.getElementById("fail-banner");
+            failBanner.classList.remove("hidden");
+            failBanner.classList.add("flex");
+            setTimeout(function () {
+              failBanner.classList.add("hidden");
+            }, 5000); // Hide after 5 seconds
+          }
+        )
+        .finally(function () {
+          // Hide loader
+          document.getElementById("loader").classList.add("hidden");
+        });
+    } else {
+      // One or both checkboxes are not checked, show an error message
+      document.getElementById("terms-banner").classList.remove("hidden")
+    }
   });
 
 // Loader
