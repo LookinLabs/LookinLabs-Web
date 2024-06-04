157683c83ee5a7eb1c4454875a952ad8a53d02d3
akinakk <artemi.baburin@tptlive.ee>
2024-06-04 15:43:30 +0300

    Some features added on JoinUs Page

---
+++ resources/pages/join-us/js/main.js
@@ -0,0 +1,83 @@
+// EmailJS
+(function () {
+  emailjs.init("ySaIap3hTf1eHrMKa");
+})();
+
+
+// Deleted functionality : NO "CONTACT US" BUTTON ANYMORE; THUS NO NEED IN HIDING THE FORM
+
+// document
+//   .getElementById("contactButton")
+//   .addEventListener("click", function (event) {
+//     event.preventDefault();
+//     let form = document.getElementById("contactForm");
+//     if (form.classList.contains("hidden")) {
+//       form.classList.remove("hidden");
+//       this.disabled = true;
+//     } else {
+//       form.classList.add("hidden");
+//       this.disabled = false;
+//     }
+//   });
+
+document
+  .getElementById("contactForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    // Check if both checkboxes are checked
+    if (document.getElementById("termsCheckbox").checked) {
+      // Show loader
+      document.getElementById("loader").classList.remove("hidden");
+
+      let formData = new FormData(this);
+      let username = formData.get("username");
+      let email = formData.get("email");
+      let title = formData.get("title");
+      let description = formData.get("description");
+      let captcha_response = grecaptcha.getResponse();
+
+      let templateParams = {
+        username: username,
+        email: email,
+        title: title,
+        description: description,
+        "g-recaptcha-response": captcha_response,
+      };
+
+      emailjs
+        .send("lookinlabs_zoho", "lookinlabs_template_id", templateParams)
+        .then(
+          function () {
+            // Hide contact form
+            document.getElementById("contactForm").classList.add("hidden");
+
+            // Show success banner
+            let successBanner = document.getElementById("success-banner");
+            successBanner.classList.remove("hidden");
+            successBanner.classList.add("flex");
+            setTimeout(function () {
+              successBanner.classList.add("hidden");
+            }, 5000); // Hide after 5 seconds
+          },
+          function (error) {
+            // Show fail banner
+            let failBanner = document.getElementById("fail-banner");
+            failBanner.classList.remove("hidden");
+            failBanner.classList.add("flex");
+            setTimeout(function () {
+              failBanner.classList.add("hidden");
+            }, 5000); // Hide after 5 seconds
+          }
+        )
+        .finally(function () {
+          // Hide loader
+          document.getElementById("loader").classList.add("hidden");
+        });
+    } else {
+      // One or both checkboxes are not checked, show an error message
+      document.getElementById("terms-banner").classList.remove("hidden");
+    }
+  });
+
+  
